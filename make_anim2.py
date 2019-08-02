6cdea8bcb407bd7b178e1079b144b3af3c67a834
Bartholomew, Claire <claire.bartholomew@metoffice.gov.uk>
2019-08-02 16:40:10 +0100

    make_anim2.py to compare against optical flow

--- make_anim2.py
+++ make_anim2.py
@@ -15,30 +15,34 @@ import iris.plot as iplt
 import pdb
 
 def animate():
-    filepath = '/scratch/cbarth/phd/img4/'
+    filepath = '/scratch/cbarth/phd/img6/'
 
-    fig = plt.figure(figsize=(12,6))
-    ax1 = plt.subplot(121)
-    ax2 = plt.subplot(122)
+    fig = plt.figure(figsize=(18,8))
+    ax1 = plt.subplot(131)
+    ax2 = plt.subplot(132)
+    ax3 = plt.subplot(133)
 
     # initiate an empty list of "plotted" images
     myimages = []
 
     # loops through available pngs
-    for batch in range(35, 39): #21, 27):
+    for batch in range(21, 27):
         print('batch = {}'.format(batch))
-        for f in range(16):
+        for f in range(14): #16):
 
             ## Read in picture
             fname1 = filepath + 'truth{}_im{}.png'.format(batch, f)
             fname2 = filepath + 'batch{}_im{}.png'.format(batch, f)
+            fname3 = filepath + 'optical_flow{}_im{}.png'.format(batch, f)
             img1 = mgimg.imread(fname1)
             img2 = mgimg.imread(fname2)
+            img3 = mgimg.imread(fname3)
 
             #imgplot = plt.imshow(img)
             imgplot1 = ax1.imshow(img1)
             imgplot2 = ax2.imshow(img2)
-            myimages.append([imgplot1, imgplot2])
+            imgplot3 = ax3.imshow(img3)
+            myimages.append([imgplot1, imgplot2, imgplot3])
 
             # append AxesImage object to the list
             #myimages.append([imgplot])
@@ -49,7 +53,7 @@ def animate():
         # save animation
         #my_anim.save('truth_anim.mp4')
         #my_anim.save('predict_anim.mp4')
-        my_anim.save('both_anim_{}.mp4'.format(batch))
+        my_anim.save('all_anim_{}.mp4'.format(batch))
 
 #===============================================================================
 def main():
--- milesial_unet.py
+++ milesial_unet.py
@@ -47,7 +47,7 @@ def prep_data(files):
         cube_data = np.append(cube_data1, cube_data2, axis=0)
         cube_data = np.append(cube_data, cube_data3, axis=0)
 
-    # Reshape data
+    # Reshape data into smaller areas (128x128) with 4 consecutive timesteps
     print(np.shape(cube_data))
     split_data_1 = np.stack(np.split(cube_data, cube_data.shape[0]/4))
     print(np.shape(split_data_1))
@@ -250,8 +250,6 @@ class down(nn.Module):
 class up(nn.Module):
     def __init__(self, in_ch, out_ch, bilinear=True):
         super(up, self).__init__()
-        #  would be a nice idea if the upsampling could be learned too,
-        #  but my machine do not have enough memory to handle all those weights
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
@@ -262,16 +260,11 @@ class up(nn.Module):
     def forward(self, x1, x2):
         x1 = self.up(x1)
 
-        # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
         x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
                         diffY // 2, diffY - diffY//2))
-
-        # for padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
 
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
